a2b2a566b92a124d8dd69a08d7e5b9126640023e
João Pedro de Oliveira <77632605+JP-Oliveira-dev@users.noreply.github.com>
2021-02-11 23:06:18 -0300

    Update randomMessage.js

--- commands/randomMessage.js
+++ commands/randomMessage.js
@@ -7,6 +7,6 @@ module.exports = function globalRandomMessage() {
   text_sd[1] = new Discord.MessageEmbed().setColor('#0000ff').setDescription('seu texto');
   text_sd[2] = new Discord.MessageEmbed().setColor('#0000ff').setDescription('seu texto');
 
-  let random = Math.floor(3* Math.random()) // lembre-se, a quantidade de textos no array tem que ser a mesma do calculo do random.
+  let random = Math.floor(3* Math.random()) 
   return text_sd[random]
 };
